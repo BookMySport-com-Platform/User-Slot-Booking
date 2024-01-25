7509d51fde9def03d3be13e12950d8fd2f283dc5
abumulla <abu.works.at.this@gmail.com>
2024-01-25 16:09:52 +0530

    initial commit

---
+++ src/main/java/com/bookmysport/userslotbooking/UserSlotBookingApplication.java
@@ -0,0 +1,13 @@
+package com.bookmysport.userslotbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSlotBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSlotBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookmysport/userslotbooking/UserSlotBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmysport.userslotbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSlotBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
