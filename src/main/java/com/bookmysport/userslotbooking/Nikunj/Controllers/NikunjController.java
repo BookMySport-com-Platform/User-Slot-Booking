b03d43ddeeafe373b5a36d069a62dd71cc3a4a48
Anandateertha.dev <vortex1649@gmail.com>
2024-05-10 15:05:25 +0530

    Add cross origin for controller

--- src/main/java/com/bookmysport/userslotbooking/Nikunj/Controllers/NikunjController.java
+++ src/main/java/com/bookmysport/userslotbooking/Nikunj/Controllers/NikunjController.java
@@ -1,7 +1,9 @@
 package com.bookmysport.userslotbooking.Nikunj.Controllers;
+
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -14,21 +16,16 @@ import com.bookmysport.userslotbooking.Nikunj.Services.DeleteSlotService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class NikunjController {
-    
-    
 
-    // @Author - Nikunj Khandelwal
-    // this is the feature to delete slot by the user who has create the slot 
-    
     @Autowired
     private DeleteSlotService deleteSlotService;
 
-    @DeleteMapping("delete")
-    public ResponseEntity<ResponseMessage> deleteSlot(@RequestHeader UUID slotId, @RequestHeader String token, @RequestHeader String role) {
+    @DeleteMapping("deleteslot")
+    public ResponseEntity<ResponseMessage> deleteSlot(@RequestHeader UUID slotId, @RequestHeader String token,
+            @RequestHeader String role) {
         return deleteSlotService.deleteSlotService(slotId, token, role);
     }
-     
-   
-}
 
+}
--- src/main/java/com/bookmysport/userslotbooking/Tarun/Controllers/TarunController.java
+++ src/main/java/com/bookmysport/userslotbooking/Tarun/Controllers/TarunController.java
@@ -4,18 +4,18 @@ import com.bookmysport.userslotbooking.Tarun.Services.GetSlotDetailsSP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TarunController {
     
     @Autowired
     private GetSlotDetailsSP Getslotdetails;
-
-    // @Author - Tarun get all endpoint for fetching all the slot id present in the database 
     
     @GetMapping("getall")
     public ResponseEntity<Object> getAllSlotIds(@RequestHeader String token,@RequestHeader String role) {
