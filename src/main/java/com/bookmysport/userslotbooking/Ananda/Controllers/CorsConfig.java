9348063cd1a5a756529512d0341fedc6c74ef054
Anandateertha.dev <vortex1649@gmail.com>
2024-05-22 10:34:42 +0530

    Add cors config file

---
+++ src/main/java/com/bookmysport/userslotbooking/Ananda/Controllers/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.bookmysport.userslotbooking.Ananda.Controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        String frontendUrl = System.getenv("FRONTEND_URL");
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(frontendUrl);
+            }
+        };
+    }
+}
