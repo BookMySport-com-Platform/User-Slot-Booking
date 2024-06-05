fb084b341b5e51f147a0d84bb367a87608e76a70
Anandateertha.dev <vortex1649@gmail.com>
2024-06-05 14:14:00 +0530

    Remove unused imports

--- src/main/java/com/bookmysport/userslotbooking/Ananda/Controllers/SlotBookController.java
+++ src/main/java/com/bookmysport/userslotbooking/Ananda/Controllers/SlotBookController.java
@@ -2,7 +2,6 @@ package com.bookmysport.userslotbooking.Ananda.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
